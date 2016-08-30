af79c4e8ae53436b0f052474c1fb95d9cb87bd7a
jame4848 <james.williams@rackspace.com>
2016-08-30 13:50:38 -0500

    python3, pep8 fixes

--- maximum_deco.py
+++ maximum_deco.py
@@ -1,116 +1,113 @@
 #!/usr/bin/env python
 
-''' A python implementation of Ratio Deco '''
+""" A python implementation of Ratio Deco """
 
-import math, argparse
+import math
+import argparse
 
-depth = ''
-bottom_time = ''
-saturation = ''
 
 def arguments():
-	global depth, bottom_time, saturation
-	parser = argparse.ArgumentParser()
-	parser.add_argument('-t', '--bt', help="Bottom Time", required=True)
-	parser.add_argument('-d', '--depth' ,help="Depth", required=True)
-	parser.add_argument('--nosaturation', help="Disable saturation of tissues", nargs='?', default=False)
-	
-	args = vars(parser.parse_args())
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        '-t',
+        '--bottom-time',
+        help='Bottom Time',
+        required=True)
+    parser.add_argument(
+        '-d',
+        '--depth',
+        help='Depth',
+        required=True)
+    parser.add_argument(
+        '--nosaturation',
+        help='Disable saturation of tissues',
+        nargs='?',
+        default=True)
+    return vars(parser.parse_args())
 
-	if args['bt']:
-		bottom_time = args['bt']
-	if args['depth']:
-		depth = args['depth']
-	if args['nosaturation'] == None:
-		saturation = False
-	else:
-		saturation = True
-
-	return depth, bottom_time, saturation
-	
-arguments()
-
-depth = int(depth)
-bottom_time = int(bottom_time)
 
 def ndl(depth):
-	if depth <= 50:
-		no_deco = 150
-	elif depth <= 60:
-		no_deco = 70
-	elif depth <= 70:
-		no_deco = 60
-	elif depth <= 80:
-		no_deco = 40
-	elif depth <= 90:
-		no_deco = 35
-	elif depth <= 100:
-		no_deco = 30
-	elif depth <= 110:
-		no_deco = 25
-	elif depth <= 120:
-		no_deco = 20
-	elif depth <= 130:
-		no_deco = 15
-	else:
-		no_deco = None
-	return no_deco
+    if depth <= 50:
+        no_deco = 150
+    elif depth <= 60:
+        no_deco = 70
+    elif depth <= 70:
+        no_deco = 60
+    elif depth <= 80:
+        no_deco = 40
+    elif depth <= 90:
+        no_deco = 35
+    elif depth <= 100:
+        no_deco = 30
+    elif depth <= 110:
+        no_deco = 25
+    elif depth <= 120:
+        no_deco = 20
+    elif depth <= 130:
+        no_deco = 15
+    else:
+        no_deco = None
+    return no_deco
+
 
 def o2_deco(depth, bottom_time):
-	ascent_time = (depth * .5) / 10
-	if depth <= 100:
-		o2_segment = float(.5 * (bottom_time - ndl(depth) + ascent_time))
-	elif depth <= 150:
-		o2_segment = .5 * bottom_time
-	elif depth <= 200:
-		o2_segment = 1 * bottom_time
-	elif depth <= 250:
-		o2_segment = 1.2 * bottom_time
-	elif depth <= 300:
-		o2_segment = 1.5 * bottom_time
-	elif depth <= 350:
-		o2_segment = 2.2 * bottom_time
-	elif depth <= 400:
-		o2_segment = 3 * bottom_time
-	else:
-		o2_segment = None
-		print "Error: The depth of", depth, "is out of the scope of this app."
-	return o2_segment
-	return depth
-	return bottom_time
+    ascent_time = (depth * .5) / 10
+    if depth <= 100:
+        o2_segment = float(.5 * (bottom_time - ndl(depth) + ascent_time))
+    elif depth <= 150:
+        o2_segment = .5 * bottom_time
+    elif depth <= 200:
+        o2_segment = 1 * bottom_time
+    elif depth <= 250:
+        o2_segment = 1.2 * bottom_time
+    elif depth <= 300:
+        o2_segment = 1.5 * bottom_time
+    elif depth <= 350:
+        o2_segment = 2.2 * bottom_time
+    elif depth <= 400:
+        o2_segment = 3 * bottom_time
+    else:
+        o2_segment = None
+        print("Error: The depth of {} is out of the scope of this app.")
+    return o2_segment
 
-o2_deco = o2_deco(depth, bottom_time)
-ndl(depth)
-first_stop = math.floor((depth * float(.5)) / 10) * 10
-first_stop = int(first_stop)
+if __name__ == '__main__':
+    args = arguments()
+    depth = int(args['depth'])
+    bottom_time = int(args['bottom_time'])
+    o2_deco = o2_deco(depth, bottom_time)
+    ndl(depth)
+    first_stop = math.floor((depth * float(.5)) / 10) * 10
+    first_stop = int(first_stop)
 
-if bottom_time <= ndl(depth):
-	print "Your dive is a no-decompression dive. Your depth is", depth, "feet and your bottom time is", bottom_time,"minutes."
-	print "Your deco profile is printed below."
-	while first_stop > 0:
-		print "Stop", first_stop, "feet for one minute."
-		first_stop = first_stop - 10
-elif depth <= 400:
-	if saturation:
-		if int(math.ceil(o2_deco)) >= 150:
-			print "Your O2 Segment is 150 minutes, therefore reached max deco and will not incur any more deco."
-			o2_deco = 150
-	print "Dive Details:"
-	print "Depth:", depth, "feet"
-	print "Bottom Time:", int(math.ceil(bottom_time)), "minutes"
-	print "First Deco Stop:", first_stop, "feet"
-	print "O2 Segment:", int(math.ceil(o2_deco)), "minutes"
-	total_deco = int(o2_deco)
-	if first_stop >= 30 <= 90:
-		seventy_segment = int(o2_deco)
-		print "70 Segment:", seventy_segment, "minutes"
-		total_deco = o2_deco + seventy_segment
-	if first_stop >= 100 <= 140:
-		onetwenty_segment = int(math.ceil(seventy_segment * .5))
-		print "120 Segment:", onetwenty_segment, "minutes"
-		total_deco = total_deco + onetwenty_segment
-	if first_stop >= 150 <= 190:
-		oneninety_segment = int(math.ceil(onetwenty_segment * .5))
-		print "190 Segment:", oneninety_segment, "minutes"
-		total_deco = total_deco + oneninety_segment
-	print "Your total deco time is", int(total_deco), "minutes."
+    if bottom_time <= ndl(depth):
+        print("Dive Details:")
+        print("Depth: {} feet".format(depth))
+        print("Bottom Time: {} minutes".format(int(math.ceil(bottom_time))))
+        while first_stop > 0:
+            print("Stop {} feet for one minute.".format(first_stop))
+            first_stop = first_stop - 10
+    elif depth <= 400:
+        if args['nosaturation']:
+            if int(math.ceil(o2_deco)) >= 150:
+                print("INFO: Saturation reached.")
+                o2_deco = 150
+        print("Dive Details:")
+        print("Depth: {} feet".format(depth))
+        print("Bottom Time: {} minutes".format(int(math.ceil(bottom_time))))
+        print("First Deco Stop: {} feet".format(first_stop))
+        print("O2 Segment: {} minutes".format(int(math.ceil(o2_deco))))
+        total_deco = int(o2_deco)
+        if first_stop >= 30 <= 90:
+            print("70 Segment: {} minutes".format(int(o2_deco)))
+            total_deco = o2_deco + int(o2_deco)
+        if first_stop >= 100 <= 140:
+            print("120 Segment: {} minutes".format(
+                int(math.ceil(int(o2_deco) * .5))))
+            total_deco = total_deco + int(math.ceil(int(o2_deco) * .5))
+        if first_stop >= 150 <= 190:
+            print("190 Segment: {} minutes".format(
+                int(math.ceil(int(math.ceil(int(o2_deco) * .5)) * .5))))
+            total_deco = total_deco + \
+                int(math.ceil(int(math.ceil(int(o2_deco) * .5))))
+        print("Total deco time is {} minutes.".format(int(total_deco)))
